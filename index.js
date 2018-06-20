c28437e32327f5a4cb530f8725104498df7e0e73
priyanshin <priyanshi.nigam@gmail.com>
2018-06-20 22:53:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!"
+  }
+    else if (distance > 2500){
+    return "No can do."
+  }
+  else if (distance > 2000){
+    return "I will gladly take your thirty bucks."
+  }
+}
+
+function ternaryCheckCity(city){
+  if (city== "NYC"){
+    return "Ok, sounds good."
+  }
+  if (city != "NYC"){
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(tip ){
+  if (tip == "generous"){
+    return "Thank you so much."
+  }
+  else if (tip == "not as generous"){
+    return "Thank you."
+    
+  }
+  else{
+    return "Bye."
+  }
+  
+  
+}
